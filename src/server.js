cb36fc211c8c884621b745253371550de95d6880
TRANG <jaykate00@gmail.com>
2023-02-09 05:28:46 +0700

    viewEngine

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import configViewEngine from './config/viewEngine';
+
+
+const app = express();
+const port = 3000
+
+configViewEngine(app);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
